4f519c5f6ef3d9e631e4401aa6fb0a7622a1507e
raghu <raghu.neerukonda@inspiredelearning.com>
2018-04-24 19:03:29 +0530

    nu file

--- linear_1.py
+++ linear_1.py
@@ -1,8 +1,19 @@
 from uaLib import *
 
+thisModule='regression/linear_1/'
+p.options.display.width=180
+
 with open('config.yaml') as yfl:
     cfg=yaml.load(yfl)
 
 datastore=cfg['datastore']
 dataset=cfg['dataset']
 dpath=dataset['path']
+f_train=dpath+thisModule+'train.csv'
+f_tst=dpath+thisModule+'tst.csv'
+
+train_set=p.read_csv(f_train)
+tst_set=p.read_csv(f_tst)
+
+train_set.dropna(inplace=True)
+tst_set.dropna(inplace=True)
