2ab7674015bd221c9b5d9b93b9d3247c2ace6d6f
raghu <raghu.neerukonda@inspiredelearning.com>
2018-04-24 19:09:25 +0530

    nu change

--- linear_1.py
+++ linear_1.py
@@ -17,3 +17,17 @@ tst_set=p.read_csv(f_tst)
 
 train_set.dropna(inplace=True)
 tst_set.dropna(inplace=True)
+
+x_train_set=train_set.as_matrix(['x'])
+y_train_set=train_set.as_matrix(['y'])
+x_tst_set=tst_set.as_matrix(['x'])
+y_tst_set=tst_set.as_matrix(['y'])
+
+print('Mean of X Training set: ',n.mean(x_train_set))
+print('Mean of Y Training set: ',n.mean(y_train_set))
+print()
+print('Median of X Training set: ',n.median(x_train_set))
+print('Median of Y Training set: ',n.median(y_train_set))
+print()
+print('S-Dev of X Training set: ',n.std(x_train_set))
+print('S-Dev of Y Training set: ',n.std(y_train_set))
