d4af90c780af60eca578bfd89d52d141c505c8bf
raghu <nraghu366@gmail.com>
2018-05-02 18:21:48 +0530

    nu updates

--- linear_1.py
+++ linear_1.py
@@ -24,12 +24,13 @@ x_tst_set=tst_set.as_matrix(['x'])
 y_tst_set=tst_set.as_matrix(['y'])
 
 print('Mean of X Training set: ',n.mean(x_train_set))
-print('Mean of Y Training set: ',n.mean(y_train_set))
-print()
 print('Median of X Training set: ',n.median(x_train_set))
-print('Median of Y Training set: ',n.median(y_train_set))
-print()
 print('S-Dev of X Training set: ',n.std(x_train_set))
+
+print()
+
+print('Mean of Y Training set: ',n.mean(y_train_set))
+print('Median of Y Training set: ',n.median(y_train_set))
 print('S-Dev of Y Training set: ',n.std(y_train_set))
 
 # Linear Regression
